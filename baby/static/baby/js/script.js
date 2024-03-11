823e13863dbdf1fbab61c67ab0f88b528d3f8e31
danny <danny@DannyX.myguest.virtualbox.org>
2024-03-11 10:19:11 +0300

    [Feature]: Messing around with static files

---
+++ baby/static/baby/js/script.js
@@ -0,0 +1,1 @@
+console.log("Does JS work also????");
--- baby/views.py
+++ baby/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("It works!!")
+    return render(request, 'baby/index.html', {})
