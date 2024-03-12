7d977f488b6a93401761bdec0bf254253102fd95
danny <danny@DannyX.myguest.virtualbox.org>
2024-03-13 00:11:24 +0300

    [Feature]: Passing JSON data as a context to the template

--- baby/views.py
+++ baby/views.py
@@ -3,4 +3,24 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html', {})
+    articles = [
+        {
+            "title": "Story of Tommy",
+            "author": "Gusto Armeda",
+            "description": "The story begins in 1980 when miami was a place of drug lords and mafia centric and there was a god.......",
+            "image" : "images/tommy.jpeg"
+        },
+        {
+            "title": "Grove Street Stories",
+            "author": "Big Smoke",
+            "description": "The story begins in 1980 when miami was a place of drug lords and mafia centric and there was a god.......",
+            "image" : "images/cj.jpeg"
+        },
+        {
+            "title": "Niko Bellic",
+            "author": "Daniel Tsega",
+            "description": "The story begins in 1980 when miami was a place of drug lords and mafia centric and there was a god.......",
+            "image" : "images/niko.jpeg"
+        }
+    ]
+    return render(request, 'index.html', {'articles': articles})
