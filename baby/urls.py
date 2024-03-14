993a4a0896ef9ccb92ee7bb17924f795ede4c729
DannySanchez6658 <danieltsega6658@gmail.com>
2024-03-14 23:01:47 +0300

    [Feture]: Messing around passing arguments in an url

--- baby/urls.py
+++ baby/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import index
+from .views import index, details
 
 urlpatterns = [
-    path("", index, name="home")
+    path("", index, name="home"),
+    path("details/<int:pk>", details, name="details")
 ]
