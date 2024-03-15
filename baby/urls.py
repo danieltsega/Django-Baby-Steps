6f3f7922a6f58b4f4cbf3833112ede981a043ad8
DannySanchez6658 <danieltsega6658@gmail.com>
2024-03-15 22:26:09 +0300

    [Feature]: Add the delete url

--- baby/urls.py
+++ baby/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import index, details, book_list, community_view, book_detail, add_book, update_book
+from .views import index, details, book_list, community_view, book_detail, add_book, update_book, delete_book
 
 urlpatterns = [
     path("", index, name="home"),
@@ -9,5 +9,6 @@ urlpatterns = [
     path("book-detail/<int:book_id>", book_detail, name="book-detail"),
     path("community/", community_view, name="community"),
     path("add_book/", add_book, name="add-book"),
-    path("update-book/<int:book_id>/", update_book, name="update-book")
+    path("update-book/<int:book_id>/", update_book, name="update-book"),
+    path("delete-book/<int:book_id>/", delete_book, name="delete-book")
 ]
