3e8f859ccf2620ccf8387a9e79f693ca58bbd6c4
DannySanchez6658 <danieltsega6658@gmail.com>
2024-03-15 10:09:26 +0300

    [Feature]: Add the book list url

--- baby/urls.py
+++ baby/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import index, details
+from .views import index, details, book_list
 
 urlpatterns = [
     path("", index, name="home"),
-    path("details/<int:pk>", details, name="details")
+    path("details/<int:pk>", details, name="details"),
+    path("book-list/", book_list, name="book-list"),
 ]
