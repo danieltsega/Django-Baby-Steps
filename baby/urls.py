4233a1a3ecc7fb2e57ce0e143a49ed9b93b0e6f1
DannySanchez6658 <danieltsega6658@gmail.com>
2024-03-15 16:20:12 +0300

    [Feature]: Add the book deatil url

--- baby/urls.py
+++ baby/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import index, details, book_list
+from .views import index, details, book_list, community_view, book_detail
 
 urlpatterns = [
     path("", index, name="home"),
     path("details/<int:pk>", details, name="details"),
     path("book-list/", book_list, name="book-list"),
+    path("book-detail/<int:book_id>", book_detail, name="book-detail"),
+    path("community/", community_view, name="community")
 ]
