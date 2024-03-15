bb708709ca191d7a09c8c6c9ce52c8ea95b08838
DannySanchez6658 <danieltsega6658@gmail.com>
2024-03-15 18:30:01 +0300

    [Feature]: Add add book and update book url

--- baby/urls.py
+++ baby/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from .views import index, details, book_list, community_view, book_detail
+from .views import index, details, book_list, community_view, book_detail, add_book, update_book
 
 urlpatterns = [
     path("", index, name="home"),
     path("details/<int:pk>", details, name="details"),
     path("book-list/", book_list, name="book-list"),
     path("book-detail/<int:book_id>", book_detail, name="book-detail"),
-    path("community/", community_view, name="community")
+    path("community/", community_view, name="community"),
+    path("add_book/", add_book, name="add-book"),
+    path("update-book/<int:book_id>/", update_book, name="update-book")
 ]
