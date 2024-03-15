e03233574fbf3b2b6cb4e5ca44f98359d3d6b365
DannySanchez6658 <danieltsega6658@gmail.com>
2024-03-15 10:08:26 +0300

    [Feature]: Adding the book model

--- baby/models.py
+++ baby/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    published_date = models.DateField()
+    
+    def __str__(self):
+        return self.title
